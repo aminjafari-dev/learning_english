405b0817a243ae9577443d0c4c6438cc294ef7fe
aminjafari-dev <110681750+aminjafari-dev@users.noreply.github.com>
2025-07-25 17:50:59 +0330

    Add Daily Lessons Dependency Injection Setup
    - Created a new dependency injection file for the Daily Lessons feature, registering necessary data sources, repository, use cases, and Bloc.
    - Integrated Daily Lessons DI setup into the main locator to ensure proper dependency management.
    - Updated localization files to include new strings for daily lessons, vocabularies, phrases, and refresh functionality, enhancing user experience.
    - Improved overall structure and organization of dependency injection for better maintainability and scalability.

--- lib/core/dependency injection/locator.dart
+++ lib/core/dependency injection/locator.dart
@@ -4,6 +4,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:learning_english/core/dependency%20injection/sign_in_di.dart';
 import 'package:learning_english/core/dependency%20injection/level_selection_di.dart';
+import 'package:learning_english/core/dependency%20injection/daily_lessons_di.dart';
+import 'package:learning_english/core/dependency%20injection/learning_focus_selection_di.dart';
 
 final getIt = GetIt.instance;
 
@@ -12,4 +14,10 @@ void initDependencies() {
   signInDi(getIt);
   // Level Selection Feature
   setupLevelSelectionDI(getIt);
+
+  // Daily Lessons Feature
+  setupDailyLessonsDI(getIt);
+
+  // Learning Focus Selection Feature
+  setupLearningFocusSelectionDI(getIt);
 }
